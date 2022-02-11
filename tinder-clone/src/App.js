798b7a191a04cb0ea24f5a5ae3c6ad0de572b0f7
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-02-10 18:14:44 -0700

    sorting folders

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,40 +1,19 @@
 import './App.css';
-import Header from './Components/Header/Header';
-import SwipeButtons from './Components/Swipe/SwipeButtons';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import TinderCards from './Components/TinderCards/TinderCards';
-import Chat from './Components/Chat/Chat';
-import Chats from './Components/Chats/Chats';
-import ChatScreen from './Components/ChatScreen/ChatScreen';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-
-        <Router>
-          <Header backButton="/" />
-          <Routes>
-            <Route path="/chat" element={<Chats />} />
-            {/* <Route path="/chat/*" element={<Chats />} /> */}
-            <Route path="/chatscreen" element={<ChatScreen />} />
-            <Route path="/tinder" element={<TinderCards />} />
-            <Route path="/" />
-          </Routes>
-
-          {/* Tinder Cards */}
-          {/*  Buttons below tinder cards */}
-          <TinderCards />
-          <SwipeButtons />
-          {/* Chat screen */}
-          {/* Individual chat screen */}
-
-
-        </Router>
-
+      <main>
+      <Router>
+      <Home />
+      </Router>
+      </main>
     </div>
   );
 }
---
+++ tinder-clone/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Chats from '../Components/Chats/Chats';
+import ChatScreen from '../Components/ChatScreen/ChatScreen';
+import TinderCards from '../Components/TinderCards/TinderCards';
+import Header from '../Components/Header/Header';
+import SwipeButtons from '../Components/Swipe/SwipeButtons';
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        <main>
+        <Routes>
+            <Route path="/chat" element={<Chats />} />
+            {/* <Route path="/chat/*" element={<Chats />} /> */}
+            <Route path="/chatscreen" element={<ChatScreen />} />
+            <Route path="/" element={<TinderCards />} />
+          </Routes>  
+        </main>
+        <SwipeButtons />
+    </div>
+  )
+}
+
+export default Home
